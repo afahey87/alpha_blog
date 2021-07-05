aa5c213a0e367edc0bdac9c2e57e4b8412bc366f
Adam Fahey <afahey87@gmail.com>
2021-07-05 09:59:55 -0400

    Added Bloggers link and edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,12 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
+        @articles = user.articles
     end
+
+    def index
+        @users = User.all
+    end 
 
     def new
         @user = User.new
